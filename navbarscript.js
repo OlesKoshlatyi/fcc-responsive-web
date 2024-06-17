61060b72c77bb8bdd84370908ca0319fdd637e04
Oles K. <lessenover@gmail.com>
2024-06-17 20:03:46 +0300

    Fix active navlink logic.

--- navbarscript.js
+++ navbarscript.js
@@ -1,22 +1,16 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll("section");
 
-let currentSection = "";
 window.addEventListener("scroll", () => {
   sections.forEach((section) => {
-    let top = window.scrollY;
-    let offset = section.offsetTop - 150;
-    let height = section.offsetHeight;
+    let scrollPosition = window.scrollY;
+    let sectionOffset = section.offsetTop - 250;
+    let sectionBottom = section.offsetHeight + sectionOffset;
     let id = section.getAttribute("id");
-    console.log("top", top);
-    console.log("offset", offset);
-    console.log("height", top);
-    console.log("id", id);
 
-    if (top >= offset && top < offset + height) {
+    if (scrollPosition >= sectionOffset && scrollPosition < sectionBottom) {
       navLinks.forEach((link) => {
         link.classList.remove("active");
-        // link.classList.add("active");
         document
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
